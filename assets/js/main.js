f489c1e772a25857d2f9f114f49ee8b5ef0ff4f6
Arnoldo23 <Arnoldokintu23@gmail.com>
2022-11-20 05:19:49 +0300

    Video Carousel Edits

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,6 +18,17 @@
 				},
 
 		};
+	
+	// Parallax.
+	function initParallax() {
+		$('#home').parallax("60%", 100);
+		$('#about').parallax("100%", 80);
+		$('#project').parallax("80%", 60);
+		$('#team').parallax("40%", 40);
+		$('#contact').parallax("20%", 20);
+		}
+	  initParallax();
+
 
 	// Breakpoints.
 		breakpoints({
